24bc9274c1078ca4aba5c1d2f313cda1e47b471b
Rocky Segarra <rocky@eskee.com>
2023-06-22 12:25:52 -0400

    meta update

--- components/meta.js
+++ components/meta.js
@@ -34,7 +34,7 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`SmartCommerce - Accelerate E-commerce from all existing touchpoints - SmartCommerce`}
+        content={`SmartCommerce - Accelerate E-commerce from all existing touchpoints`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
--- pages/blog.js
+++ pages/blog.js
@@ -16,7 +16,7 @@ export default function Index({ preview, allPosts }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>{`SmartCommerce - Accelerate E-commerce from all existing touchpoints`}</title>
         </Head>
         <Intro />
         <Container>
