fef069b42eb37186ccb46a033b0bc2e4b79b49f3
Rocky Segarra <rocky@eskee.com>
2023-06-26 13:37:47 -0400

    added newsletter page

---
+++ pages/newsletter.js
@@ -0,0 +1,42 @@
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Layout from "../components/layout";
+import { getAllPostsForHome } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import Newsletter from "../components/newsletter";
+import HeroHero from "../components/HeroHero";
+
+export default function Index({ preview, allPosts }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title>{`SmartCommerce - Accelerate E-commerce from all existing touchpoints`}</title>
+        </Head>
+        <h2 className="font-normal leading-tight mb-20 p-5 bg-white border-t shadow-sm">
+          <div className="mx-auto lg:max-w-7xl sm:max-w-7xl sm:px-6 lg:px-8 px-10 sm:px-8 text-sm">
+            <h1 className="text-3xl md:text-3xl font-bold md:pr-8 text-dark-950">
+              Newsletter<span className="text-secondary-950">.</span>
+            </h1>
+          </div>
+        </h2>
+        <Container>
+          <Newsletter />
+          <style>{"body { background-color: #f5f5f7; }"}</style>
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps({ preview = false }) {
+  const allPosts = (await getAllPostsForHome(preview)) ?? [];
+  return {
+    props: { preview, allPosts },
+  };
+}
