b3c98817b096a9c4922a926f92380e408559ba61
Zangetsu <sgchavada2013@gmail.com>
2023-07-08 21:04:44 +0530

    implement pagination

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,7 +1,6 @@
 import DateComponent from "./date";
 
 export default function NewsletterCard({ posts }) {
-  console.log(posts)
   return (
     <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3 sm:grid-cols-2 mb-16">
       {posts.map((post) => (
