57c5c9beba03e494c619bb83e66cfa8ea821ad96
Rocky Segarra <rocky@eskee.com>
2023-08-08 17:12:53 -0400

    update

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Wrapper from "../components/Wrapper";
+import Container from "../components/Container";
 import Heading from "../components/Heading";
 import Paragraphs from "../components/Paragraphs";
 import Button from "../components/Button";
