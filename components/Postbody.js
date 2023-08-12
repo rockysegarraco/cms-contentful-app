0bd6694daa62250402f5e6b8cc8ad4e735d404a8
SagarChavada <sgchavada2013@gmail.com>
2023-08-12 10:59:56 +0530

    remove log

--- components/Postbody.js
+++ components/Postbody.js
@@ -15,9 +15,6 @@ const customMarkdownOptions = (content) => ({
 });
 
 export default function PostBody({ content }) {
-  console.log('====================================');
-  console.log(content);
-  console.log('====================================');
   return (
     <>
       <div className="max-w-3xl mx-auto text-dark-950 font-normal">
