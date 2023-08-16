93995060e73e187e30946fb79f0de3cab0f8a297
Rocky Segarra <rockysegarra@icloud.com>
2023-08-16 17:57:14 -0400

    update calculator

--- pages/calculator.js
+++ pages/calculator.js
@@ -2,7 +2,7 @@ export default function _Primary(props) {
   const { children } = props; // Destructure 'children' from 'props'
   return (
     <div className={[props.className]}>
-      <div className="mx-auto max-w-7xl px-8 py-16 sm:px-6 sm:py-32">
+      <div className="mx-auto max-w-6xl px-8 py-16 sm:px-6 sm:py-32">
         {children}
       </div>
     </div>
