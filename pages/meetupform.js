081ce70c7628070a2b08aa8450ab1849bc7bdb16
Rocky Segarra <rockysegarra@icloud.com>
2023-08-31 20:40:58 -0400

    Update meetupform.js

--- pages/meetupform.js
+++ pages/meetupform.js
@@ -26,19 +26,19 @@ export default function Example() {
             content="Contact us For general inquiries, please use the form below. Locations US Offices: Atlanta, GA Email:&nbsp;info@smartcommerce.co Phone:&nbsp;1-800-571-3520 Europe: Barcelona, Spain Email: infoEU@smartcommerce.co"
           />
         </Head>
-        <Intro title="Schedule a Meetup" />
+        <Intro title="Schedule a Meet up" />
         <Wrapper slate>
           <_Container onecolumn>
             <div className="text-start">
               <div className="mx-auto max-w-xl">
                 <div className="mb-8">
                   <h2 className="text-3xl font-bold text-info-950 mb-4">
-                    Let's meetup!
+                    Let's meet up!
                   </h2>
                   <Paragraphs>
                     Are you attending an event we will be at too? If so, we'd
                     love to chat about how we can help you drive more commerce.
-                    Fill out the form below to schedule a meet-up with one of
+                    Fill out the form below to schedule a meet up with one of
                     the SmartCommerce team members!
                   </Paragraphs>
                 </div>
