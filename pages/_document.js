03e1f9b08773810e4eb4400a8b4e20bb90d0c633
Rocky Segarra <rockysegarra@icloud.com>
2023-09-13 14:58:23 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,28 +18,24 @@ export default function Document() {
           type="text/javascript"
           src="https://cdn-cookieyes.com/client_data/8cddd9e07b2f233dbfda8450/script.js"
         ></script>
+        {/* Tag manager script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=GTM-K6MBFWV"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K6MBFWV');
-            `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'GTM-K6MBFWV');
+              `,
           }}
         ></script>
-        {/* Google Analytics */}
       </Head>
       <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K6MBFWV"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
         <Main />
         <NextScript />
       </body>
