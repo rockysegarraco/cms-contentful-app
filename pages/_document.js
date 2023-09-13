8774245ba146dcd43030302787df64f6fb4e26bb
Rocky Segarra <rockysegarra@icloud.com>
2023-09-13 14:33:49 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,11 +23,17 @@ export default function Document() {
           async
           src="https://www.googletagmanager.com/gtag/js?id=GTM-K6MBFWV"
         ></script>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K6MBFWV');</script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'GTM-K6MBFWV');
+              `,
+          }}
+        ></script>
       </Head>
       <body>
         <Main />
