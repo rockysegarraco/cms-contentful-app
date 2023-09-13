3a995c7b843f58256e449034c2b1ceb1de007857
Rocky Segarra <rockysegarra@icloud.com>
2023-09-13 14:33:06 -0400

    update

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,16 @@ export default function Document() {
           type="text/javascript"
           src="https://cdn-cookieyes.com/client_data/8cddd9e07b2f233dbfda8450/script.js"
         ></script>
+        {/* Tag manager script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=GTM-K6MBFWV"
+        ></script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-K6MBFWV');</script>
       </Head>
       <body>
         <Main />
