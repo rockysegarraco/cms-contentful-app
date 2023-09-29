c227f7ae5190475eccd986a7e6c1fc45c55104b4
Rocky Segarra <rockysegarra@icloud.com>
2023-09-29 14:23:30 -0400

    Update DynamicForm.js

--- components/DynamicForm.js
+++ components/DynamicForm.js
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const FormDemo = () => {
   useEffect(() => {
     // Function to check if the element with data-qp-target-id exists
     const checkElement = () => {
-      const element = document.getElementById('fa-form');
-      if (element && element.hasAttribute('data-qp-target-id')) {
+      const element = document.getElementById("fa-form");
+      if (element && element.hasAttribute("data-qp-target-id")) {
         // Create the script element when the element is found
-        const script = document.createElement('script');
-        script.src = 'https://www.tfaforms.com/publish/4988154';
-        script.setAttribute('data-qp-target-id', 'fa-form');
+        const script = document.createElement("script");
+        script.src = "https://www.tfaforms.com/publish/4988154";
+        script.setAttribute("data-qp-target-id", "fa-form");
         script.defer = true;
 
         // Append the script to the document's body to load it
@@ -25,7 +25,9 @@ const FormDemo = () => {
 
     // Cleanup: remove the script when the component unmounts
     return () => {
-      const scriptElement = document.querySelector('script[src="https://www.tfaforms.com/publish/4988154"]');
+      const scriptElement = document.querySelector(
+        'script[src="https://www.tfaforms.com/publish/4988154"]'
+      );
       if (scriptElement) {
         document.body.removeChild(scriptElement);
       }
@@ -35,6 +37,12 @@ const FormDemo = () => {
   return (
     <div>
       <div id="fa-form"></div>
+      <iframe
+        src="https://tfaforms.com/4988154"
+        height="700"
+        width="100%"
+        frameborder="0"
+      ></iframe>
       {/* Add any other content or components related to your form */}
     </div>
   );
