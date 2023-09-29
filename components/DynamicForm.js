8badf00426a1fe2c5225528f4b7a1a758cd9690e
SagarChavada <sgchavada2013@gmail.com>
2023-09-29 23:14:33 +0530

    fix issue

--- components/DynamicForm.js
+++ components/DynamicForm.js
@@ -6,7 +6,8 @@ const FormDemo = () => {
     console.log("its calling this form")
     // Create a script element for the external script
     const script = document.createElement('script');
-    script.src = 'https://www.tfaforms.com/4988154';
+    script.src = 'https://www.tfaforms.com/publish/4988154';
+    script.setAttribute('data-qp-target-id', 'fa-form');
     script.defer = true;
 
     // Append the script to the document's body to load it
@@ -21,7 +22,7 @@ const FormDemo = () => {
   return (
     <div>
       <h1>Form demo</h1>
-      {/* <div id="fa-form"></div> */}
+      <div id="fa-form"></div>
       {/* Add any other content or components related to your form */}
     </div>
   );
