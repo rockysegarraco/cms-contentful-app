5fb5fa33b339c4c711ae95b3ef303fad826a2b1a
SagarChavada <sgchavada2013@gmail.com>
2023-09-29 23:31:47 +0530

    fix recaptcha issue

--- components/DynamicForm.js
+++ components/DynamicForm.js
@@ -1,31 +1,40 @@
-// components/FormDemo.js
-//import { useEffect } from 'react';
-import Head from "next/head";
+import { useEffect } from 'react';
 
 const FormDemo = () => {
-  // useEffect(() => {
-  //   // Create a script element for the external script
-  //   const script = document.createElement('script');
-  //   script.src = 'https://www.tfaforms.com/publish/4988154';
-  //   script.setAttribute('data-qp-target-id', 'fa-form');
-  //   script.defer = true;
+  useEffect(() => {
+    // Function to check if the element with data-qp-target-id exists
+    const checkElement = () => {
+      const element = document.getElementById('fa-form');
+      if (element && element.hasAttribute('data-qp-target-id')) {
+        // Create the script element when the element is found
+        const script = document.createElement('script');
+        script.src = 'https://www.tfaforms.com/publish/4988154';
+        script.setAttribute('data-qp-target-id', 'fa-form');
+        script.defer = true;
 
-  //   // Append the script to the document's body to load it
-  //   document.body.appendChild(script);
+        // Append the script to the document's body to load it
+        document.body.appendChild(script);
+      } else {
+        // Check again after a short delay
+        setTimeout(checkElement, 100);
+      }
+    };
 
-  //   // Cleanup: remove the script when the component unmounts
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
+    // Start checking for the element
+    checkElement();
+
+    // Cleanup: remove the script when the component unmounts
+    return () => {
+      const scriptElement = document.querySelector('script[src="https://www.tfaforms.com/publish/4988154"]');
+      if (scriptElement) {
+        document.body.removeChild(scriptElement);
+      }
+    };
+  }, []);
 
   return (
     <div>
-      <Head>
-        <script src="https://www.tfaforms.com/publish/4988154"  data-qp-target-id="fa-form" defer></script>
-      </Head>
       <div id="fa-form"></div>
-
       {/* Add any other content or components related to your form */}
     </div>
   );
