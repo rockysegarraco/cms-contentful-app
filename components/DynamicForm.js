c5d121de1598df8e5c6ee2919f38fff828eb8e6c
SagarChavada <sgchavada2013@gmail.com>
2023-09-29 23:26:13 +0530

    fix attribute issue

--- components/DynamicForm.js
+++ components/DynamicForm.js
@@ -22,9 +22,9 @@ const FormDemo = () => {
   return (
     <div>
       <Head>
-        <script src="https://www.tfaforms.com/publish/4988154" defer></script>
+        <script src="https://www.tfaforms.com/publish/4988154"  data-qp-target-id="fa-form" defer></script>
       </Head>
-      
+      <div id="fa-form"></div>
 
       {/* Add any other content or components related to your form */}
     </div>
