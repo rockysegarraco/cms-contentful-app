986b786a0d0ac1b19a8366b0e7273e2cd9468e2d
Rocky Segarra <rockysegarra@icloud.com>
2023-11-24 13:17:23 -0500

    Update events.js

--- pages/events.js
+++ pages/events.js
@@ -9,6 +9,8 @@ import Clients from "../components/partners/clients";
 import { fetchNewsletter } from "../lib/api";
 import Head from "next/head";
 import Link from "next/link";
+import Heading from "../components/Heading";
+import Button from "../components/Button";
 import {
   BuildingOffice2Icon,
   EnvelopeIcon,
@@ -30,21 +32,36 @@ export default function Example() {
           />
         </Head>
         <_PageTitle title="Events" />
-        <_Hero
-          title="Shop Talk 2024"
-          ButtonTitle="Learn more"
-          ButtonLink="https://shoptalk.com/us"
-          ImageUrl="img/events/shop-talk.jpg"
-        >
-          <h1 className="text-xl">
-            March 17th-20th 2024 <br /> at Mandalay Bay Las Vegas.
-          </h1>
-          <p className="pt-4">
-            For four days, top execs from innovative brands and retailers gather
-            in Las Vegas to discuss the evolving landscape of consumer
-            discovery, shopping, and purchasing.
-          </p>
-        </_Hero>
+        <Wrapper slate>
+          <_Container twocolumn>
+            <div className="order-last lg:order-first">
+              <Heading dark>
+                Shop Talk 2024
+                <span className="text-secondary-950">.</span>
+              </Heading>
+
+              <Paragraphs dark>
+                <span className="font-semibold">
+                  March 17th-20th 2024 at Mandalay Bay Las Vegas.
+                </span>{" "}
+                <br />
+                <br />
+                For four days, top execs from innovative brands and retailers
+                gather in Las Vegas to discuss the evolving landscape of
+                consumer discovery, shopping, and purchasing.
+              </Paragraphs>
+              <Link href="https://shoptalk.com/us">
+                <Button primary>Learn more</Button>
+              </Link>
+            </div>
+            <div className="relative w-full order-first lg:order-last">
+              <img
+                src="img/events/shop-talk.jpg"
+                className="relative rounded-2xl w-full"
+              />
+            </div>
+          </_Container>
+        </Wrapper>
         <div className="bg-white lg:bg-slate-50 py-0 lg:py-16">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto mt-8 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3 sm:grid-cols-2 mb-16">
@@ -58,7 +75,7 @@ export default function Example() {
                   <div class="font-bold text-xl mb-2 text-info-950">
                     GroceryShop
                   </div>
-                  <div className="text-dark-950 pb-2 text-sm">
+                  <div className="text-dark-950 pb-2 text-sm font-semibold">
                     Oct 7th-9th 2024
                     <br /> at Mandalay Bay Las Vegas.
                   </div>
