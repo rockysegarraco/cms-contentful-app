2b5699357804af5db5f1ddefc64ffafab755d123
Rocky Segarra <rockysegarra@icloud.com>
2024-02-12 14:11:01 -0500

    Update demo-beauty.js

--- pages/demo-beauty.js
+++ pages/demo-beauty.js
@@ -68,7 +68,7 @@ export default function Example() {
             </div>
             <div
               aria-hidden="true"
-              className="absolute inset-0 bg-gray-900 opacity-50"
+              className="absolute inset-0 bg-gray-900 opacity-10"
             />
             <div className="relative mx-auto flex max-w-6xl flex-col items-center px-6 py-32 text-center sm:py-64 lg:px-0">
               <h1 className="text-4xl font-bold text-white lg:text-7xl mb-8">
