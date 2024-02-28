ee39d42e392157467948287b80820164f9f97393
Rocky Segarra <rockysegarra@icloud.com>
2024-02-28 08:05:58 -0500

    Update success-demo-google.js

--- pages/success-demo-google.js
+++ pages/success-demo-google.js
@@ -10,11 +10,11 @@ export default function Example() {
     <>
       <main className="bg-white px-0 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <h1 className="mt-0 text-3xl leading-[38px] lg:leading-[4rem] font-bold tracking-tight text-secondary-950 sm:text-5xl">
+          <h1 className="mt-0 text-4xl leading-[38px] lg:leading-[4rem] font-bold tracking-tight text-secondary-950 sm:text-5xl">
             Can’t wait to connect!
           </h1>
           <p className="mt-6 text-base leading-7 text-slate-950">
-            We’ll be in touch soon.
+            We’ll be in touch soon...
           </p>
         </div>
       </main>
