01709b4b09ada2453a146820e40aab193ee703b2
Rocky Segarra <rockysegarra@icloud.com>
2024-03-28 09:07:14 -0400

    added toaster banner

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import Footer from "../components/footer";
 import Meta from "../components/meta";
 import Nav from "../components/nav";
+import Toaster from "../components/toaster";
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
       <Nav />
+      <Toaster />
       <div className="min-h-screen">
         <main>{children}</main>
       </div>
